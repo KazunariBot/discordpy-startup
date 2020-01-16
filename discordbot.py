9edf21d6453451a0c51729d44f32dcc573a569cc
KazunariBot <59944749+KazunariBot@users.noreply.github.com>
2020-01-16 16:23:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,18 +12,36 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
 @bot.command()
 async def abuki(ctx):
-    await ctx.send('GG Boyz:たいじ,ダイナモン,えとな,レン(Ren〆Zone)')
+    await ctx.send('GG Boyz:たいじ(シューター),ダイナモン(ラピ/バレ/ナモ/エク),えとな(シューター/バケ凸/キャンプ),Ren〆Zone(バケ凸/L3)')
     
 @bot.command()
 async def bbuki(ctx):
-    await ctx.send('Libalent Clamari:くろす,あとばる,ぴょん,2438学園')    
+    await ctx.send('Libalent Clamari:くろす(ブラ系/バケ凸),あとばる(シューター),ぴょん(チャー系),2438学園(黒ZAP/黒マニュ)')    
     
+@bot.command()
+async def cbuki(ctx):
+    await ctx.send('Cool&Cool:むしきんぐ(シューター),反射神経(クアッド),うどん店長(チャー系),のりすけ(H3D/わかば)')  
+    
+@bot.command()
+async def dbuki(ctx):
+    await ctx.send('波乱万丈:ろんつ(スクスロ),はんじょう(スプロラ),ku(マニュコラ),バズ(スピナー)')
+    
+@bot.command()
+async def ebuki(ctx):
+    await ctx.send('TASO:ゆっきー(ロンブラ),かよたそ(キャンプ/クアッド/青スパ),ガワタ(スシコラ/デュアカス),ぱいなぽ～(ブラ系/チャー系)')      
+    
+@bot.command()
+async def fbuki(ctx):
+    await ctx.send('DetonatioN Gaming:ミリンケーキ(傘/L3),ku(マニュコラ/L3D),れき(ジェット/エク/バレ),けいとぅーん(バケ凸)')    
 
+@bot.command()
+async def gbuki(ctx):
+    await ctx.send('ハイパービーム:テルミ(パブロ系),けいとぅーん(バケ凸),しめぴぃ(傘/スプスピ),ミルクレープ(スコ有チャー/リッター)')     
+
+@bot.command()
+async def hbuki(ctx):
+    await ctx.send('王の運搬クエスト:もこう(カー凸),たいじ(96凸),りんごもちぃ(96凸),ティラミス(96凸)')    
+    
 bot.run(token)
