f5d47605cd51ac0949e1feac430756fd8d32b972
KazunariBot <59944749+KazunariBot@users.noreply.github.com>
2020-01-16 18:16:41 +0900

    12

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -11,33 +12,34 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
 
-@bot.command()
-async def abuki(ctx):
-    await ctx.send('GG BoyZ:たいじ(シューター),ダイナモン(ラピ/バレ/ナモ/エク),えとな(シューター/バケ凸/キャンプ),Ren〆Zone(L3)')
-    
-@bot.command()
-async def bbuki(ctx):
-    await ctx.send('Libalent Clamari:くろす(ブラ系/バケ凸),あとばる(シューター),ぴょん(チャー系),2438学園(黒ZAP/黒マニュ)')    
-    
-@bot.command()
-async def cbuki(ctx):
-    await ctx.send('BBV TOKYO:むしきんぐ(シューター),反射神経(クアッド),うどん店長(チャー系),のりすけ(H3D/わかば)')  
-    
-@bot.command()
-async def dbuki(ctx):
-    await ctx.send('波乱万丈:ろんつ(スクスロ),はんじょう(スプロラ),ku(マニュコラ),バズ(スピナー)')
-    
-@bot.command()
-async def ebuki(ctx):
-    await ctx.send('TASO:ゆっきー(ロンブラ),かよたそ(キャンプ/クアッド/青スパ),ガワタ(スシコラ/デュアカス),ぱいなぽ～(ブラ系/チャー系)')      
-    
-@bot.command()
-async def fbuki(ctx):
-    await ctx.send('DetonatioN Gaming:ミリンケーキ(傘/L3),ku(マニュコラ/L3D),れき(ジェット/エク/バレ),けいとぅーん(バケ凸/キャンプ)')    
+@bot.event    
+async def on_message(message): 
 
-@bot.command()
-async def gbuki(ctx):
-    await ctx.send('王の運搬クエスト(スプラ1より):もこう(カー凸),たいじ(96凸),りんごもちぃ(96凸),ティラミス(96凸)')    
+ if message.content == "チーム":
+    
+   team = [
+    
+    'GG BoyZ:たいじ(シューター),ダイナモン(ラピ/バレ/ナモ/エク),えとな(シューター/バケ凸/キャンプ),Ren〆Zone(L3)',
+    
+    'Libalent Clamari:くろす(ブラ系/バケ凸),あとばる(シューター),ぴょん(チャー系),2438学園(黒ZAP/黒マニュ)',
+    
+    'BBV TOKYO:むしきんぐ(シューター),反射神経(クアッド),うどん店長(チャー系),のりすけ(H3D/わかば)',
+    
+    '波乱万丈:ろんつ(スクスロ),はんじょう(スプロラ),ku(マニュコラ),バズ(スピナー)', 
+    
+    'TASO:ゆっきー(ロンブラ),かよたそ(キャンプ/クアッド/青スパ),ガワタ(スシコラ/デュアカス),ぱいなぽ～(ブラ系/チャー系)',
+
+    'DetonatioN Gaming:ミリンケーキ(傘/L3),ku(マニュコラ/L3D),れき(ジェット/エク/バレ),けいとぅーん(バケ凸/キャンプ)', 
+
+    '王の運搬クエスト(スプラ1より):もこう(カー凸),たいじ(96凸),りんごもちぃ(96凸),ティラミス(96凸)']
+
+
+  choice = random.choice(team)
+
+    
+ await message.send_message(message.channel, choice)
+
     
 bot.run(token)
