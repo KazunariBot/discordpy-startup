c051b71cf8d3cc99e56fd52c6b1f8168306f9bc8
KazunariBot <59944749+KazunariBot@users.noreply.github.com>
2020-01-16 18:26:36 +0900

    12

--- discordbot.py
+++ discordbot.py
@@ -15,31 +15,13 @@ async def on_command_error(ctx, error):
     
 
 @bot.event    
-async def on_message(message): 
+async def team(ctx): 
+    
+   team = ['GG BoyZ', 'Libalent Clamari', 'BBV TOKYO', '波乱万丈', 'TASO', 'DetonatioN Gaming', '王の運搬クエスト(スプラ1より)']
 
- if message.content == "チーム":
+   choice = random.choice(team)
     
-   team = [
-    
-    'GG BoyZ:たいじ(シューター),ダイナモン(ラピ/バレ/ナモ/エク),えとな(シューター/バケ凸/キャンプ),Ren〆Zone(L3)',
-    
-    'Libalent Clamari:くろす(ブラ系/バケ凸),あとばる(シューター),ぴょん(チャー系),2438学園(黒ZAP/黒マニュ)',
-    
-    'BBV TOKYO:むしきんぐ(シューター),反射神経(クアッド),うどん店長(チャー系),のりすけ(H3D/わかば)',
-    
-    '波乱万丈:ろんつ(スクスロ),はんじょう(スプロラ),ku(マニュコラ),バズ(スピナー)', 
-    
-    'TASO:ゆっきー(ロンブラ),かよたそ(キャンプ/クアッド/青スパ),ガワタ(スシコラ/デュアカス),ぱいなぽ～(ブラ系/チャー系)',
-
-    'DetonatioN Gaming:ミリンケーキ(傘/L3),ku(マニュコラ/L3D),れき(ジェット/エク/バレ),けいとぅーん(バケ凸/キャンプ)', 
-
-    '王の運搬クエスト(スプラ1より):もこう(カー凸),たいじ(96凸),りんごもちぃ(96凸),ティラミス(96凸)']
-
-
-  choice = random.choice(team)
-
-    
- await message.send_message(message.channel, choice)
+await message.send_message(message.channel, choice)
 
     
 bot.run(token)
